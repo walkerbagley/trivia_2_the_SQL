8eccebf0a0f1a1ac046c283a34fbe1e07d08f659
zbrown31 <zachary.brown@ridedesi.com>
2024-10-29 22:34:00 -0400

    add view set

--- trivia_2_api/trivia/urls.py
+++ trivia_2_api/trivia/urls.py
@@ -1,7 +1,13 @@
+from xml.etree.ElementInclude import include
 from django.urls import path
+
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
+router = DefaultRouter()
+router.register(r"questions", views.QuestionViewSet)
+
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", include(router.urls)),
 ]
--- trivia_2_api/trivia/views.py
+++ trivia_2_api/trivia/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from .models import Question
+
 
 # Create your views here.
 
 def index(request):
 
     return HttpResponse("Hello Trivia People")
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    def list(self, request):
+        queryset = Question.objects.all()   
+        serializer = QuestionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, id=None):
+        queryset = Question.objects.all()
+        question = get_object_or_404(queryset, id=id)
+        serializer = QuestionSerializer(user)
+        return Response(serializer.data)
