d685728ce6d37b110eeca82eabf368c5ec49d7ee
Dagny Brand <dagnybrand@Dagnys-MacBook-Pro-2.local>
2024-11-02 16:13:29 -0400

    minor changes to App

--- trivia-2-the-sql/src/App.js
+++ trivia-2-the-sql/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Main from './Components/Main/Main.js'
 import PreHost from './Components/Host/Host.js'
-import PreGame from './Components/Play/Play.js'
+import PrePlay from './Components/Play/Play.js'
 import Login from './Components/Login/Login.js'
 import Decks from './Components/Decks/Decks.js'
 import Account from './Components/Account/Account.js'
 import Header from './Components/Header/Header.js'
 import AuthForm from './Components/Auth/AuthForm.js';
 import AuthRegister from './Components/Auth/AuthRegister.js';
-import Parse from 'parse'
-import {Routes, Route} from 'react-router-dom'
-import { useState } from 'react';
+import Parse from 'parse';
+import {Routes, Route} from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import './variables.css'
 
@@ -25,6 +25,18 @@ initializeParse(
 let isLoggedIn = false;
 
 const App = () => {
+  const url = "";
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => { 
+    return fetch(url) 
+            .then((res) => res.json()) 
+            .then((d) => setData(d)) 
+    }
+    
+    useEffect(() => {
+      fetchInfo();
+    }, [])
 
     return (
       <div>
@@ -32,7 +44,7 @@ const App = () => {
         <Routes>
           <Route exact path='/' element={<Main/>}/>
           <Route exact path='/Host' element={<PreHost/>}/>
-          <Route exact path='/Game' element={<PreGame/>}/>
+          <Route exact path='/Play' element={<PrePlay/>}/>
           <Route exact path='/Login' element={<Login/>}/>
           <Route exact path='/Decks' element={<Decks/>}/>
           <Route exact path='/Account' element={<Account/>}/>
