45304b2c5f68d327908b66fe60db53717756b2ad
zbrown31 <zachary.brown@ridedesi.com>
2024-11-06 12:30:43 -0500

    user id to jwt

--- trivia_2_api/routers/auth.py
+++ trivia_2_api/routers/auth.py
@@ -47,7 +47,7 @@ async def login_for_access_token(
    
             access_token_expires = datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
             access_token = create_access_token(
-                data={"sub": user.user_name}, expires_delta=access_token_expires
+                data={"sub": str(user.id)}, expires_delta=access_token_expires
             )
             return Token(token=access_token, token_type="bearer")
 
