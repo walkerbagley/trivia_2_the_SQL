164abeca100a34744662b7e5a5d2ca106fece646
Walker Bagley <wbagley@nd.edu>
2024-11-05 23:52:26 -0500

    teams on account page

--- trivia-2-the-sql/src/Components/Account/AccountService.js
+++ trivia-2-the-sql/src/Components/Account/AccountService.js
@@ -1,17 +1,13 @@
-import axios from "axios";
-
 const api = 'http://127.0.0.1:8000/'
 
-export const getTeams = () => {
+export const getTeams = (axios) => {
     let config = {
         method: 'get',
-        url: api + 'team',
-        headers: { 
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ3YmFnbGV5IiwiZXhwIjoxNzMxMDQyNDIwfQ.FWKSc7vhfTnUTOquQ-zvM_7MeKGU6VetWUtE2d_EAMA'
-        }
+        url: '/team/'
+        // 'Authentication': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ3YmFnbGV5IiwiZXhwIjoxNzMxMDQ3MDQ4fQ.m18gTFfvEnoKtfS7yh4kO5ugj6UEyI43nS0oyvQ254o'
     };
 
-    axios.request(config).then((response) => {
+    axios.get('/team/').then((response) => {
         console.log(JSON.stringify(response.data));
         return response;
     }).catch((error) => {
