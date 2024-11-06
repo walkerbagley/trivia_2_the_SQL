99fd02dc2534a106d2fbd4aa78c967fbdade9db3
zbrown31 <zachary.brown@ridedesi.com>
2024-11-05 19:51:18 -0500

    add cors middleware

--- trivia_2_api/main.py
+++ trivia_2_api/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .middleware import authenticate_user
 from .routers import auth, deck, game, host, question, score, team, user
@@ -12,6 +13,13 @@ async def lifespan(instance: FastAPI):
     await db.close()
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(deck.router)
