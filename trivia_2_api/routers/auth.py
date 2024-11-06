e0cf3602c9a7681585fb57d5628a1250be731343
zbrown31 <zachary.brown@ridedesi.com>
2024-11-06 13:26:18 -0500

    login on register

--- trivia_2_api/routers/auth.py
+++ trivia_2_api/routers/auth.py
@@ -1,8 +1,6 @@
 import datetime
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
-from psycopg.rows import class_row
-from typing import Annotated
+from fastapi import APIRouter, HTTPException, status
+from psycopg.rows import class_row, dict_row
 
 
 from ..db import db
@@ -17,20 +15,29 @@ router = APIRouter(
 )
 
 @router.post("/register")
-async def register(user: RegisterRequest) -> None:
+async def register(request: RegisterRequest) -> None:
+    print('request', request)
     async with db.connection() as conn:
-        async with conn.cursor(row_factory=class_row(RegisterResponse)) as cur:
-            await cur.execute('''SELECT id, user_name, hashed_password FROM "Users" WHERE user_name = %s''', (user.username,))
-            user = await cur.fetchone()
-            if user is not None:
-                raise HTTPException(
-                    status_code=status.HTTP_409_CONFLICT,
-                    detail="Username already exists",
+            async with conn.cursor(row_factory=class_row(RegisterResponse)) as cur:
+                await cur.execute('''SELECT id, user_name, hashed_password FROM "Users" WHERE user_name = %s''', (request.username,))
+                user = await cur.fetchone()
+                print('user in func ', user)
+                if user is not None:
+                    raise HTTPException(
+                        status_code=status.HTTP_409_CONFLICT,
+                        detail="Username already exists",
+                    )
+            async with conn.cursor(row_factory=dict_row) as cur: 
+                await cur.execute('''INSERT INTO "Users" (user_name, hashed_password) VALUES (%s, %s) RETURNING id''',
+                                (request.username, hash_password(request.password)))
+                id =  (await cur.fetchone()).get('id')
+                access_token_expires = datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+                access_token = create_access_token(
+                    data={"sub": str(id)}, expires_delta=access_token_expires
                 )
-            
-            await cur.execute('''INSERT INTO "Users" (user_name, hashed_password) VALUES (%s, %s) RETURNING id''',
-                              (user.username, hash_password(user.password)))
-            return await cur.fetchone()
+                return Token(token=access_token, token_type="bearer")
+        
+
 
 @router.post("/login")
 async def login_for_access_token(
