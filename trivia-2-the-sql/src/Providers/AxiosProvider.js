e241e5c83c1e9dbcdaa181e591ae1d6e853321c5
zbrown31 <zachary.brown@ridedesi.com>
2024-11-05 22:56:29 -0500

    add axios import

--- trivia-2-the-sql/src/Providers/AxiosProvider.js
+++ trivia-2-the-sql/src/Providers/AxiosProvider.js
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useEffect, useMemo } from 'react';
 import { useAuthSession } from './AuthProvider';
+import axios from 'axios';
 
 const defaultAxiosInstance = axios.create({
   baseURL: 'http://127.0.0.1:8000', 
