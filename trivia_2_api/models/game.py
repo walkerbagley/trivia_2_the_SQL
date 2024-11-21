00087c8122473bb2a5394529b8090720ef7bbb8a
zbrown31 <zachary.brown@ridedesi.com>
2024-11-21 11:31:58 -0500

    add join code to games

--- trivia_2_api/models/game.py
+++ trivia_2_api/models/game.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from shlex import join
 from pydantic import BaseModel
 from typing import Optional
 from uuid import UUID
@@ -7,11 +8,10 @@ class Game(BaseModel):
     id: UUID
     deck_id: UUID
     host_id: UUID
+    join_code: str
     start_time: datetime
     end_time: Optional[datetime] = None
 
 class GameRequest(BaseModel):
     deck_id: UUID
-    host_id: UUID
     start_time: datetime
-    end_time: Optional[datetime] = None
--- trivia_2_api/routers/game.py
+++ trivia_2_api/routers/game.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter, HTTPException
+from shlex import join
+import string
+
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import JSONResponse
 from psycopg.rows import class_row, dict_row
+from random import choices
 from uuid import UUID
 
 from ..db import db
@@ -16,25 +20,26 @@ router = APIRouter(
 async def get_game() -> list[Game]:
     with db.connection() as conn:
         with conn.cursor(row_factory=class_row(Game)) as cur:
-            cur.execute('''SELECT id, deck_id, host_id, start_time, end_time FROM "Games"''')
+            cur.execute('''SELECT id, deck_id, host_id, join_code, start_time, end_time FROM "Games"''')
             return cur.fetchall()
 
 @router.get("/{id}")
 async def get_game(id: UUID) -> Game:
     with db.connection() as conn:
         with conn.cursor(row_factory=class_row(Game)) as cur:
-            cur.execute('''SELECT id, deck_id, host_id, start_time, end_time FROM "Games" WHERE id = %s''', (id,))
+            cur.execute('''SELECT id, deck_id, host_id, join_code, start_time, end_time FROM "Games" WHERE id = %s''', (id,))
             question = cur.fetchone() 
             if question is None:
                 raise HTTPException(status_code=404, detail="Game not found")
             return question
 
 @router.post("/")
-async def create_game(game: GameRequest) -> None:
+async def create_game(request: Request, game: GameRequest) -> None:
     with db.connection() as conn:
         with conn.cursor(row_factory=dict_row) as cur:
-            cur.execute('''INSERT INTO "Games" (deck_id, host_id, start_time, end_time) VALUES (%s, %s, %s, %s) RETURNING id''',
-                              (game.deck_id, game.host_id, game.start_time, game.end_time))
+            join_code = ''.join(choices(string.ascii_uppercase + string.digits, k=6))
+            cur.execute('''INSERT INTO "Games" (deck_id, host_id, start_time, join_code) VALUES (%s, %s, %s, %s) RETURNING id''',
+                              (game.deck_id, request.state.user.id, game.start_time, join_code))
             game_id = (cur.fetchone()).get("id", None)
             if game_id is None:
                 raise HTTPException(status_code=500, detail="Failed to create game")
@@ -45,8 +50,8 @@ async def create_game(game: GameRequest) -> None:
 async def update_game(id: UUID, game: GameRequest) -> None:
     with db.connection() as conn:
         with conn.cursor() as cur:
-            cur.execute('''UPDATE "Games" SET deck_id = %s, host_id = %s, start_time = %s, end_time = %s WHERE id = %s''',
-                              (game.deck_id, game.host_id, game.start_time, game.end_time, id))
+            cur.execute('''UPDATE "Games" SET deck_id = %s, host_id = %s, join_code = %s, start_time = %s, end_time = %s WHERE id = %s''',
+                              (game.deck_id, game.host_id, game.join_code, game.start_time, game.end_time, id))
 
 @router.delete("/{id}")
 async def delete_game(id: UUID) -> None:
