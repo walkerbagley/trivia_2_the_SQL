9e353588ef2f02e65c9f93185272f7d3b5c795c0
Patrick Schlosser <pschloss@nd.edu>
2024-12-16 17:36:11 -0500

    scores page

--- trivia-2-the-sql/src/Components/Game/Scores/ScorePage.js
+++ trivia-2-the-sql/src/Components/Game/Scores/ScorePage.js
@@ -1,10 +1,15 @@
-
+import { useNavigate } from "react-router-dom";
 
 const ScorePage = () => {
-
+    const navigate = useNavigate();
     return (
         <div>
             Scores
+            <button
+                className="button-52"
+                onClick={navigate('/')}>
+                Home
+            </button>
         </div>
     );
 
